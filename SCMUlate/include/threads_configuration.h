f048769626de9a8e3661a8326d4601033e7c59ae
Jose Manuel Monsalve Diaz <josem@udel.edu>
2019-08-23 15:40:45 -0500

    Adding gitignore.
    Adding definitions for Abstract MAchine definition

---
+++ SCMUlate/include/threads_configuration.h
@@ -0,0 +1,4 @@
+#define SU_THREAD 0
+#define MEM_THREAD 1
+#define CU_THREADS case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10: case 11:
+
--- SCMUlate/proof_of_concept.c
+++ SCMUlate/proof_of_concept.c
@@ -4,13 +4,23 @@
 #include "SCMUlate_tools.h"
 #include "machine_configuration.h"
 
+// FUNCTION DEFINITIONS
+int SCMUlate ();
+int SCMUlate_SU_Behavior();
+int SCMUlate_MEM_Behavior();
+int SCMUlate_CU_Behavior();
+
 int main () {
+
+    // === INITIALIZATION ====
     // We check the register configuration is valid
     if(!checkRegisterConfig()) {
         return 1;
     }
     describeRegisterFile();
 
+    // === EMULATION STARTING ===
+    SCMULATE_ERROR_IF(SCMUlate() != 0, "ERROR EXECUTING SCMUlate() function");
     return 0;
 }
 
@@ -18,7 +28,20 @@ int SCMUlate () {
 
 #pragma omp parallel
     {
+        #pragma omp master 
+        {
+            SCMULATE_INFOMSG("Running with %d threads ", omp_get_num_threads());
+        }
         switch (omp_get_thread_num()) {
+            case SU_THREAD:
+                SCMUlate_SU_Behavior();
+                break;
+            case MEM_THREAD:
+                SCMUlate_MEM_Behavior();
+                break;
+            CU_THREADS
+                SCMUlate_CU_Behavior();
+                break;
             default:
                 SCMULATE_WARNING("Threa created with no purpose. What's my purpose? You pass the butter");
         }
@@ -28,5 +51,12 @@ int SCMUlate () {
     return 0;
 }
 
-
-
+int SCMUlate_SU_Behavior() {
+    return 0;
+}
+int SCMUlate_MEM_Behavior() {
+    return 0;
+}
+int SCMUlate_CU_Behavior() {
+    return 0;
+}

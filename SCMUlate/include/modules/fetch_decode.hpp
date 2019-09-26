c90fe5c3b8ea8c1ea746ab992d5901a90ae524cd
Jose Monsalve <josem@udel.edu>
2019-09-26 18:43:57 -0500

    Partial compiling and starting with fetch_decode.cpp

--- SCMUlate/include/modules/fetch_decode.hpp
+++ SCMUlate/include/modules/fetch_decode.hpp
@@ -2,7 +2,24 @@
 #define __FETCH_DECODE__
 
 #include "SCMUlate_tools.hpp"
+#include "instruction_mem.hpp"
 
+namespace scm {
 
-int SCMUlate_SU_Behavior();
+  class fetch_decode_module {
+    private:
+      inst_mem_module * inst_mem_m;
+      bool * alive; 
+      int PC; 
+  
+      fetch_decode_module() = delete;
+      fetch_decode_module(inst_mem_module * const inst_mem);
+
+      int behavior();
+  
+    public: 
+  }
+  
+}
+
 #endif
--- SCMUlate/src/modules/fetch_decode.cpp
+++ SCMUlate/src/modules/fetch_decode.cpp
@@ -1,5 +1,14 @@
 #include "fetch_decode.hpp"
 
-int SCMUlate_SU_Behavior() {
-    SCMULATE_INFOMSG(1, "I am an SU");
+scm::fetch_decode_module::fetch_decode_module(inst_mem_module * const inst_mem, bool * const alive_sig) {
+  this->inst_mem_m = inst_mem;
+  this->PC = 0;
+  this->alive = alive_sig;
+}
+
+int
+scm::fetch_decode_module::behavior() {
+  SCMULATE_INFOMSG(1, "I am an SU");
+  while (*(this->alive_sig)) { }
+    return 0;
 }

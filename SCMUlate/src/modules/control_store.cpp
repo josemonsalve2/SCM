ee0ad6464b3492a7795ca122f4403c25b373e568
Jose M Monsalve Diaz <jmonsalvediaz@anl.gov>
2023-07-16 11:02:47 -0500

    cherry picked race condition fix from resiliency

--- SCMUlate/src/modules/control_store.cpp
+++ SCMUlate/src/modules/control_store.cpp
@@ -1,24 +1,32 @@
 #include "control_store.hpp"
 
-bool 
-scm::execution_slot::try_insert(scm::instruction_state_pair *newInstruction) {
-  #pragma omp flush
+bool scm::execution_slot::try_insert(
+    scm::instruction_state_pair *newInstruction) {
+
   if ((*tail) == nullptr) {
-    *this->tail = newInstruction;
+    auto t = this->tail;
+#pragma omp atomic write
+    *t = newInstruction;
     getNext(tail);
+#pragma omp flush
     return true;
   } else {
     return false;
   }
-
 }
 
 void 
 scm::execution_slot::consume() {
-  (*head)->second = instruction_state::EXECUTION_DONE;
-  *this->head = nullptr;
-  #pragma omp flush release 
+  auto &to_end = (*head)->second;
+
+  instructions_queue_t h;
+#pragma omp atomic read
+  h = this->head;
+#pragma omp atomic write
+  *h = nullptr;
   getNext(head);
+#pragma omp flush
+  to_end = instruction_state::EXECUTION_DONE;
 }
 
 scm::control_store_module::control_store_module(const int numExecUnits) {
@@ -30,7 +38,7 @@ scm::control_store_module::control_store_module(const int numExecUnits) {
 
 scm::control_store_module::~control_store_module() {
   // Deleting the execution slots
-  for (auto it = this->execution_slots.rbegin(); it < this->execution_slots.rend(); ++it)
+  for (auto it = this->execution_slots.rbegin();
+       it != this->execution_slots.rend(); ++it)
     delete (*it);
-
 }
